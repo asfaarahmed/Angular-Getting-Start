8820cef0d857073f9209989069defe5e7f7cf772
asfaaramed <asfaarahmed93@gmail.com>
2016-05-08 18:50:36 +0500

    Angular 2 getting start

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>{{name}}s First Angular 2 App</h1>'
+})
+export class AppComponent {
+  
+  name :string = "Asfaar";
+  
+ }
